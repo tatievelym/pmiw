226301c2de760dc78c8a420bc4d394c6b479971a
Sokol Tobias <tbssokol.55@gmail.com>
2025-11-13 21:51:23 -0300

    c

--- tpfinalparte2/pantallas.js
+++ tpfinalparte2/pantallas.js
@@ -12,7 +12,14 @@ class pantallas {
     this.imagenes[2] = loadImage('data/teclado.webp');
   }
 
-  mostrarInicio() {
-    image(this.imagenes[0], 0, 0, width, height);
+  mostrar() {
+    let p = 'inicio';
+    if (p = 'inicio') {
+      image(this.imagenes[0], 0, 0, width, height);
+      console.log('pantalla '+p )
+    }
+    if (p = 'controles'){
+    image(this.imagenes[1], 0, 0, width, height);
+    }
   }
 }
--- tpfinalparte2/tpfinalparte2.js
+++ tpfinalparte2/tpfinalparte2.js
@@ -17,7 +17,7 @@ function setup () {
 }
 
 function draw () {
-  pantalla.mostrarInicio();
+  pantalla.mostrar();
   jugador.dibujar();
 }
 
